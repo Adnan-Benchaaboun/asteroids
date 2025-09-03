847af274614a7f31a19558c7fab9fdde37d41506
Adnan-Benchaaboun <adnanben@comcast.net>
2025-09-03 15:35:35 -0400

    Added Asteroids and shooting logic

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import pygame
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 from constants import *
+import sys
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, drawable, updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     dt = 0
 
     while True:
@@ -13,10 +32,21 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+            
+        for obj in asteroids:
+            if obj.collision_detection(player):
+                sys.exit("Game Over!")
+            
         pygame.display.flip()
-        time = clock.tick(60)
-        dt = time / 1000
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
